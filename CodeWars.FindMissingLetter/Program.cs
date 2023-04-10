3477432020ddd6be9fe4d048c0400f5d7e4a749c
Samet UCA <sametuca@hotmail.com>
2023-04-10 18:41:48 +0300

    FindMissingLetter problem solving

---
+++ CodeWars.FindMissingLetter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars.FindMissingLetter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static char FindMissingLetter(char[] array)
+        {
+            var alphabet = char.IsUpper(array.First()) ? "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToList() : "abcdefghijklmnopqrstuvwxyz".ToList();
+            var indexSearch = alphabet.IndexOf(array.First());
+
+            for (int i = 0; i < alphabet.Count; i++)
+            {
+                if (array[i] != alphabet[indexSearch])
+                    return alphabet[indexSearch];
+
+                indexSearch++;
+            }
+
+            return ' ';
+
+        }
+    }
+}
