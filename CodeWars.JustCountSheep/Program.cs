de1537d9d61b2468941acc0d0e42fb13d6c21dc2
Samet UCA <sametuca@hotmail.com>
2023-04-15 20:44:30 +0300

    problem solved "just count sheep" algoritm

---
+++ CodeWars.JustCountSheep/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeWars.JustCountSheep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var returnValue = CountSheep(3);
+            Console.WriteLine(returnValue);
+            Console.ReadLine();
+        }
+        public static string CountSheep(int n)
+        {
+            List<string> list = new List<string>();
+            if (n == 1) {
+                return "1 sheep...";
+            }
+            for (int i = 0; i < n; i++)
+            {
+                list.Add($"{i+1} sheep...");
+            }
+            string joinedString = string.Join("", list);
+            return joinedString;
+        }
+    }
+}
