f323c8efe5513ac2e954484c626e3bbc26ddb4d6
Samet UCA <sametuca@hotmail.com>
2023-04-18 20:51:26 +0300

    DigPow solved

--- CodeWars.DigPow/Program.cs
+++ CodeWars.DigPow/Program.cs
@@ -1,6 +1,25 @@
 ﻿using System;
 using System.Linq;
+/*
+ Some numbers have funny properties. For example:
 
+89 --> 8¹ + 9² = 89 * 1
+
+695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+
+we want to find a positive integer k, if it exists, such that the sum of the digits of n taken to the successive powers of p is equal to k * n.
+In other words:
+
+Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+If it is the case we will return k, if not return -1.
+
+Note: n and p will always be given as strictly positive integers.
+ */
 namespace CodeWars.DigPow
 {
     internal class Program
@@ -8,10 +27,15 @@ namespace CodeWars.DigPow
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            digPow2(24, 2);
+            Console.WriteLine(digPow2(89, 1));
+        }
+        public static long digPow1(int n, int p)
+        {
+            var sum = Convert.ToInt64(n.ToString().Select(s => Math.Pow(int.Parse(s.ToString()), p++)).Sum());
+            return sum % n == 0 ? sum / n : -1;
         }
 
-        public static long digPow(int n, int p)
+        public static long digPow2(int n, int p)
         {
             char[] charArray = n.ToString().ToCharArray();
             int[] intArray = new int[charArray.Length];
@@ -27,20 +51,23 @@ namespace CodeWars.DigPow
             return (long)math;
         }
 
-        public static long digPow2(int n, int p)
+        public static long digPow3(int n, int p)
         {
-            char[] charArray = n.ToString().ToCharArray();
-            int[] intArray = new int[charArray.Length];
-            for (int i = 0; i <= intArray.Length - 1; i++)
+            char[] digits = n.ToString().ToCharArray();
+            long sum = 0;
+            for (int i = 0; i < digits.Length; i++)
             {
-                intArray[i] = int.Parse(charArray[i].ToString());
+                sum += (long)Math.Pow(Char.GetNumericValue(digits[i]), p + i);
             }
-            double math = 0;
-            for (int i = 0; i < intArray.Length; i++)
+            long k = sum / n;
+            if (sum % n == 0)
             {
-                math += Math.Pow(intArray[i], ++p);
+                return k;
             }
-            return (long)math * p;
+            else
+            {
+                return -1;
+            }
         }
     }
 }
