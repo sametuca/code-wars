0975efadf21a465b9e0956d54499bbee33b392e1
sametuca <sametuca@hotmail.com>
2023-04-19 15:24:05 +0300

    DeleteOccurencesElement solved

---
+++ CodeWars.DeleteOccurrencesElement/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.DeleteOccurrencesElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 1, 3, 3, 7, 2, 2, 2, 2 };
+            var result = DeleteNth(arr, 3);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+        
+        public static int[] DeleteNth(int[] arr, int x)
+        {
+            // girdi dizisindeki öğeleri filtreler
+            return arr.Where((e, i) => 
+                // her öğenin sayısını sayar
+                arr.Take(i + 1).Count(x => x == e) <= x
+            ).ToArray(); // sonucu int[] türünde bir dizi olarak döndürür
+        }
+
+
+        public static int[] DeleteNth2(int[] arr, int x)
+        {
+            var result = new List<int>();
+            var seen = new HashSet<int>();
+
+            foreach (int item in arr)
+            {
+                if (seen.Count(i => i == item) < x)
+                {
+                    result.Add(item);
+                    seen.Add(item);
+                }
+            }
+
+            return result.ToArray();
+            return arr.GroupBy(e => e).SelectMany(e => e.Take(x)).ToArray();
+        }
+    }
+}
+
---
+++ CodeWars.KataExampleTwist/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeWars.KataExampleTwist
+{
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+        public static string[] Websites
+        {
+            get
+            {
+                return Enumerable.Range(0,1000).Select(e => "codewars").ToArray();
+            }
+        }
+    }
+}
+
