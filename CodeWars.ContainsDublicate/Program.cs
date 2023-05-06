d7662a9dc927056432f0979eb2a8557a53b2d0d6
Samet UCA <sametuca@hotmail.com>
2023-05-06 18:52:51 +0300

    added CDublicate solution fixed

---
+++ CodeWars.ContainsDublicate/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeWars.ContainsDublicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var returnBooleanVal = ContainsDuplicate(new int[] { 1, 2, 3, 1 });
+            Console.WriteLine(returnBooleanVal);
+            Console.ReadKey();
+        }
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            return nums.Length != nums.Distinct().Count();
+        }
+    }
+}
