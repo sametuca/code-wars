fe0714fee570c118be3f7e256ff132aca7fa567c
Samet UCA <sametuca@hotmail.com>
2023-05-09 21:00:20 +0300

    added soundex string problem solved

---
+++ CodeWars.Soundex/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeWars.Soundex
+{
+    internal class Program
+    {
+        //CodeWars : https://www.codewars.com/kata/587319230e9cf305bb000098/train/csharp
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static Dictionary<char, char> table = new Dictionary<char, char>
+            {
+              {'b','1'},{'f','1'},{'p','1'}, {'v','1'},
+              {'c','2'},{'g','2'},{'j','2'},{'k','2'},{'q','2'},{'s','2'},{'x','2'}, {'z','2'},
+              {'d','3'},{'t','3'},
+              {'l','4'},
+              {'m','5'},{'n','5'},
+              {'r','6'}
+            };
+
+        public static string Soundex(string names)
+        {
+            return String.Join(" ", names.Split(" ").Select(s => ProcessString(s).ToUpper()));
+        }
+
+        public static string ProcessString(string s)
+        {
+            char f1 = s[0];
+
+            s = s.ToLower();
+
+            s = s[0].ToString() + Regex.Replace(s.Substring(1), "[hw]", "");
+
+            s = String.Join("", s.Select(e => table.ContainsKey(e) ? table[e] : e));
+
+            s = Regex.Replace(s, @"(\d)(\1)+", m => m.Value[0].ToString());
+
+            s = s[0].ToString() + Regex.Replace(s.Substring(1), "[aeiouy]", "");
+
+            if (Char.IsNumber(s[0]))
+                s = f1 + s.Substring(1);
+
+            return Regex.Matches(s, @"\d").Count() < 3 ? (s + "000").Substring(0, 4) : s.Substring(0, 4);
+        }
+    }
+}
