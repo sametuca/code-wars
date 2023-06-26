af8de141f4e70dcd8ff775e690c9494e539f55e2
Samet UCA <sametuca@hotmail.com>
2023-06-26 19:40:26 +0300

    solved whodominates problem

---
+++ CodeWars.ArrayDominates/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace CodeWars.ArrayDominates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            WhoDominates(new int[] { 3, 4, 3, 2, 3, 1, 3, 3 });
+        }
+        public static int WhoDominates(int[] array)
+        {
+            Dictionary<int,int> dic = new Dictionary<int,int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (dic.ContainsKey(array[i]))
+                    dic[array[i]]++;
+                else
+                    dic.Add(array[i], 1);    
+            }
+            KeyValuePair<int, int> maxPair = new KeyValuePair<int, int>();
+            foreach (KeyValuePair<int, int> pair in dic)
+            {
+                if (pair.Value > maxPair.Value)
+                {
+                    maxPair = pair;
+                }
+            }
+            if(maxPair.Value > (array.Length / 2))
+            {
+                return maxPair.Key;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+    }
+}
