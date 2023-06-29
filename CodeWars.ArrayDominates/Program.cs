93aa23eed3347a6c42d0b990ab5eebc40209d404
Samet UCA <sametuca@hotmail.com>
2023-06-29 21:06:14 +0300

    added arrayDominates problem paragraph

--- CodeWars.ArrayDominates/Program.cs
+++ CodeWars.ArrayDominates/Program.cs
@@ -1,14 +1,24 @@
 ﻿namespace CodeWars.ArrayDominates
 {
+    /**
+     * 
+     * DESCRIPTION:
+        A zero-indexed array arr consisting of n integers is given. The dominator of array arr is the value that occurs in more than half of the elements of arr.
+        For example, consider array arr such that arr = [3,4,3,2,3,1,3,3]
+        The dominator of arr is 3 because it occurs in 5 out of 8 elements of arr and 5 is more than a half of 8.
+        Write a function dominator(arr) that, given a zero-indexed array arr consisting of n integers, returns the dominator of arr. The function should return −1 if array does not have a dominator. All values in arr will be >=0.
+     * 
+     * 
+     * */
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
             WhoDominates(new int[] { 3, 4, 3, 2, 3, 1, 3, 3 });
         }
         public static int WhoDominates(int[] array)
         {
+       
             Dictionary<int,int> dic = new Dictionary<int,int>();
             for (int i = 0; i < array.Length; i++)
             {
@@ -35,5 +45,18 @@
             }
 
         }
+        // 2. Solution
+        public static int WhoDominates2(int[] array)
+        {
+
+            var groups = array.GroupBy(a => a);
+            if (groups.Max(g => g.Count()) > array.Length / 2)
+            {
+                return groups.OrderByDescending(g => g.Count()).First().Key;
+            }
+            return -1;
+
+        }
+
     }
 }
