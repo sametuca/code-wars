c411332cfa8999e78176e3831fdde0e2116ef661
Samet UCA <sametuca@hotmail.com>
2023-07-03 19:50:20 +0300

    array diff kullanilmayan degisken silindi

--- CodeWars.ArrafDiff/Program.cs
+++ CodeWars.ArrafDiff/Program.cs
@@ -16,7 +16,6 @@ namespace CodeWars.ArrayDiff
         }
         public static int[] ArrayDiff(int[] a, int[] b)
         {
-            int[] arr = new int[a.Length];
             for (int i = 0; i < b.Length; i++)
             {
                 if (a[i] == b[i])
