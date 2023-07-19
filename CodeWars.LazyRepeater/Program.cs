5df3f045427aa9ef71374060bd206e2fadb344cd
Samet UCA <sametuca@hotmail.com>
2023-07-19 21:46:20 +0300

    added lazy repeater solved

---
+++ CodeWars.LazyRepeater/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+ The makeLooper() function (or make_looper in your language) takes a string (of non-zero length) as an argument. It returns a function. The function it returns will return successive characters of the string on successive invocations. It will start back at the beginning of the string once it reaches the end.
+
+For example:
+
+Func<char> abc = Kata.MakeLooper("abc");
+abc(); // should return 'a' on this first call
+abc(); // should return 'b' on this second call
+abc(); // should return 'c' on this third call
+abc(); // should return 'a' again on this fourth call
+ 
+ */
+
+static Func<char> MakeLooper(string sequence)
+{
+    int index = 0;
+    return () =>
+    {
+        char result = sequence[index];
+        index = (index + 1) % sequence.Length;
+        return result;
+    };
+}
+
