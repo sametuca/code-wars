a49171595ccbd7f08db81d47b31c396271e96b52
Samet UCA <sametuca@hotmail.com>
2023-07-21 19:23:49 +0300

    added call function lazyrepeater solved

--- CodeWars.LazyRepeater/Program.cs
+++ CodeWars.LazyRepeater/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 /*
  The makeLooper() function (or make_looper in your language) takes a string (of non-zero length) as an argument. It returns a function. The function it returns will return successive characters of the string on successive invocations. It will start back at the beginning of the string once it reaches the end.
@@ -14,6 +13,11 @@ abc(); // should return 'a' again on this fourth call
  
  */
 
+Func<char> makeLooperFunc = MakeLooper("123");
+var firstChar = makeLooperFunc();
+var second = makeLooperFunc();
+var third = makeLooperFunc();
+Console.WriteLine($"First char {firstChar}\nSecond char {second}\nThird char {third}");
 static Func<char> MakeLooper(string sequence)
 {
     int index = 0;
