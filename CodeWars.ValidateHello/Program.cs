4623cf1b4178da2f0c85f5e06b817405b3661add
Samet UCA <sametuca@hotmail.com>
2023-12-02 21:44:30 +0300

    added ValidateHello

---
+++ CodeWars.ValidateHello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace CodeWars.ValidateHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var result = Validate_hello("Hallo,");
+        }
+
+        public static bool Validate_hello(string greetings)
+        {
+            var hello = new string[] { "hello", "ciao", "salut", "hallo", "hola", "ahoj", "czesc" };
+            var result = hello.Any(x => greetings.ToLower().Contains(x));
+            return result;
+        }
+    }
+}
