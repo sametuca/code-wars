13e429008b670946a6ad28f39af727a985580a4d
Samet UCA <sametuca@hotmail.com>
2023-12-02 21:26:12 +0300

    WhoseMove added

---
+++ CodeWars.WhoseMove/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace CodeWars.WhoseMove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static string WhoseMoveShort(string l, bool w) => w ? l : l == "white" ? "black" : "white";
+
+        public static string WhoseMove(string lastPlayer, bool win)
+        {
+            if(lastPlayer == "White")
+            {
+                if(win)
+                {
+                    return "White";
+                }
+                else
+                {
+                    return "Black";
+                }
+            }
+            else
+            {
+                if(win)
+                {
+                    return "Black";
+                }
+                else
+                {
+                    return "White";
+                }
+            }
+        }
+    }
+}
